3f9612c5536d09aecb27813c6334a34e8c5ba2ed
Mega <Aza.Noriega@gmail.com>
2016-06-21 10:40:18 +0500

    Add package.json, switch to using Promises

--- compile.js
+++ compile.js
@@ -1,13 +1,18 @@
 'use strict';
 
-let exec = require('child_process').exec,
+// dependencies
+let path = require('path'),
 	colors = require('colors'),
-	path = require('path'),
-	file = process.argv[2],
-	filename = path.basename(file, '.hbs');
+	exec = require('child_process').exec;
 
-function compile() {
+// make some data to work with
+let file = process.argv[2];
+
+// general function for compiling a single Handlebars file when its changed
+function compile(file) {
+	let filename = path.basename(file, '.hbs');
 	exec(`handlebars ${file} -f ./compiled/${filename}.js`, function (err, stdout, stderr) {
+		// TODO: add concatination and minification
 		console.log(`Handlebars: ${filename} compiled`.cyan);
 		if (err) console.error(`Error: ${err}`);
 		if (stdout) console.log(`stdout: ${stdout}`);
@@ -15,4 +20,5 @@ function compile() {
 	});
 }
 
-compile();
+Promise.resolve(file)
+	.then(compile);
